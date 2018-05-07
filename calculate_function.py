ab1b36bad2da607b6f14fefbedc2d5019c53e1c7
Goliaita <davide.basile01@gmail.com>
2018-05-07 20:01:04 +0200

    oggi basta

--- calculate_function.py
+++ calculate_function.py
@@ -2,29 +2,22 @@ import numpy as np
 
 
 def quadraticF(x, Q, r):
-    x_t = x
-    r_t = np.zeros(1, len(x))
-    for i in range(0, len(x)):
-        x_t[1][i] = x[i]
 
-    for i in range(0, len(r)):
-        r_t[1][i] = r[i]
-
-
-    f = 1/ 2 * np.dot(np.dot(x_t, Q), x) + np.dot(r_t, x)
+    f = 1 / 2 * np.dot(np.dot(np.transpose(x), Q), x) + np.dot(np.transpose(r), x)
 
     return f
 
 
 def gradientF(x, Q, r):
-    for i in range(0, len(Q)):
-        for j in range(0, len(Q)):
-            Q_t[i][j] = Q[j][i]
 
-    df = 1 / 2 * np.dot(Q_t, x) + 1 / 2 * np.dot(Q, x) + r
+    df = 1 / 2 * np.dot(np.transpose(Q), x) + 1 / 2 * np.dot(Q, x) + r
 
     return df
 
 
-def newPositionX(U_i, X, Q, r, alpha):
-    return U_id - alfa * gradientF(X, Q, r);
+def newPositionX(xk, X, Q, r, alpha):
+
+    xk_1 = xk - alpha * gradientF(X, Q, r)
+
+    return xk_1
+
--- create_matrix.py
+++ create_matrix.py
@@ -5,58 +5,33 @@ import numpy as np
 
 
 def createAdjM(d):
-    G = nx.Graph()
+    g = nx.DiGraph()
     for i in range(0, d):
-        G.add_node(i)
-
-    for i in range(0, d):
-        G.add_edge(i, i+1)
-
-    Adj = nx.adjacency_matrix(G)
-
-    return Adj
+        g.add_node(i)
+    for j in range(0, d):
+        if j == d-1:
+            g.add_edge(j, 0)
+        else:
+            g.add_edge(j, j + 1)
+    adj = nx.adjacency_matrix(g)
+    return adj
 
 
 def createR(d):
     r = np.zeros(d)
-    for x in range(0, d):
-        r[x] = randint(0, 1)
-
+    for i in range(0, d):
+        r[i] = randint(0, 1)
     return r
 
 
 def createQ(d):
-    Adj = np.zeros((d, d))
-    A = np.zeros((d, d))
+    q = np.zeros((d, d))
     eye = np.identity(d)
-    for x in range(0, d):
- [4. 3. 0. 2. 7.]]
-[1. 0. 0. 0. 0.]
-[[0 1 0 0]
-        for y in range(0, d):
-            Adj[x][y] = randint(0, 2)
-            if x == y:
-                eye[x][y] = 1
-            else:
-                eye[x][y] = 0
-
-    for i in range(d):
-        for j in range(d):
-            A = np.add(Adj, np.transpose(Adj))
-
-            A = np.add(A, np.dot(d, eye))
-
-    return A
+    for i in range(0, d):
+        for j in range(0, d):
+            q[i][j] = randint(0, 2)
+    a = np.add(q, np.transpose(q))
+    a = np.add(a, np.dot(d, eye))
+    return a
 
 
-if __name__ == '__main__':
-
-    x = createQ(5)
-
-    y = createR(5)
-
-    z = createAdjM(3)
-
-    print(x)
-    print(y)
-    print(z.todense())
